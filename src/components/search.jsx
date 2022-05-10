96768914f9af123bf5953fcff88b2f4dbd78930b
SvetlanaRiasnova <R_S_U@outlook.com>
2022-05-10 22:04:29 +1000

    project complete

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -12,55 +12,55 @@ class Search extends React.Component {
         }
     };
     handleFilter = (event) => {
-        this.setState(() => ({type: event.target.dataset.type}), ()=> {
+        this.setState(() => ({ type: event.target.dataset.type }), () => {
             this.props.searchMovies(this.state.search, this.state.type)
         });
     }
     render() {
-        return        <div className="row">
+        return <div className="row">
             <div className="col s12">
                 <div className="input-field ">
-                    <input 
-                        className="validate" 
-                        placeholder='search' 
-                        type="search" 
+                    <input
+                        className="validate"
+                        placeholder='search'
+                        type="search"
                         value={this.state.search}
-                        onChange={(e) => this.setState({search: e.target.value})}
+                        onChange={(e) => this.setState({ search: e.target.value })}
                         onKeyDown={this.handleKey}
                     />
-                    <button 
-                        className='btn search-btn purple lighten-3' 
+                    <button
+                        className='btn search-btn purple lighten-3'
                         onClick={() => this.props.searchMovies(this.state.search, this.state.type)}>Search
                     </button>
                 </div>
                 <div>
                     <label>
-                        <input 
-                            className="with-gap purple" 
-                            name="group3" type="radio" 
-                            data-type="all" 
+                        <input
+                            className="with-gap purple"
+                            name="group3" type="radio"
+                            data-type="all"
                             onChange={this.handleFilter}
                             checked={this.state.type === 'all'}
                         />
                         <span>All</span>
                     </label>
                     <label>
-                        <input 
-                            className="with-gap purple" 
-                            name="group3" 
-                            type="radio" 
-                            data-type="movie" 
+                        <input
+                            className="with-gap purple"
+                            name="group3"
+                            type="radio"
+                            data-type="movie"
                             onChange={this.handleFilter}
                             checked={this.state.type === 'movie'}
                         />
                         <span>Movie</span>
                     </label>
                     <label>
-                        <input 
-                            className="with-gap purple" 
-                            name="group3" 
-                            type="radio" 
-                            data-type="series" 
+                        <input
+                            className="with-gap purple"
+                            name="group3"
+                            type="radio"
+                            data-type="series"
                             onChange={this.handleFilter}
                             checked={this.state.type === 'series'}
                         />
@@ -72,4 +72,4 @@ class Search extends React.Component {
     }
 }
 
-export {Search}
+export { Search }
